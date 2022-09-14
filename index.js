9c832ef0a95138586c412123dacfd26c32cdc14f
YaekOne <daniramirez@hotmail.com.ar>
2022-09-13 21:52:53 -0500

    Last version

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function desencriptar() {
     var cont = 0;
     var cont2 = 0;
     var txtOut = "";
-    
+
     var dictEncrypt = {
         a: "ai",
         e: "enter",
@@ -52,44 +52,48 @@ function desencriptar() {
     var txtOut = "";
 
 
-    for (var i=0; i<textin.length;i++ ) {
-        //console.log("i: "+ i)
+    for (var i = 0; i < textin.length; i++) {
+        console.log("i: " + i)
         var letDict;
         var tamLetDict;
-        var swt = false;        
+        var swt = false;
 
         Object.keys(dictEncrypt).forEach(prop => {//recorre las llaves
-            
-            if (textin[i] == prop) {//si la letra es igual a alguna llave
-                //console.log("swt");
-                var letDict = dictEncrypt[prop];
-                var tamLetDict = dictEncrypt[prop].length;
-                if (textin.length - i >= tamLetDict) {//no busque valores menores a los del diccionario
-                    //console.log("swt2");
-                    for (var i2 of letDict) {//itera sobre el valor del diccionario
-                        //console.log("textin[cont2]: " + textin[i+cont]);
-                        //console.log("i2: " + i2);
-                        if (textin[i+cont] == i2) {//compara los valores de entrada con los del diciionario
-                            cont++;                            
-                            //console.log("swt3");
-                            if (cont == tamLetDict) {//completo la palabra
-                                txtOut += prop;
-                                cont = 0;
-                                //console.log("swt4");
-                                if(i<textin.length){//si la posicion de busqueda es menor va saltando la busqueda
-                                    i+=tamLetDict-1                                    
-                                    swt = true;                                    
-                                };
+            if (!swt) {
+                if (textin[i] == prop) {//si la letra es igual a alguna llave
+                    //console.log("swt: " + i);
+                    var letDict = dictEncrypt[prop];
+                    var tamLetDict = dictEncrypt[prop].length;
+                    if (textin.length - i >= tamLetDict) {//no busque valores menores a los del diccionario
+                        //console.log("swt2");
+                        for (var i2 of letDict) {//itera sobre el valor del diccionario
+                            //console.log("textin[cont2]: " + textin[i + cont]);
+                            //console.log("i2: " + i2);
+                            if (textin[i + cont] == i2) {//compara los valores de entrada con los del diciionario
+                                cont++;
+                                //console.log("swt3");
+                                if (cont == tamLetDict) {//completo la palabra
+                                    txtOut += prop;
+                                    cont = 0;
+                                    //console.log("swt4: " + i);
+                                    //console.log("prop: " + prop);
+                                    if (i < textin.length) {//si la posicion de busqueda es menor va saltando la busqueda
+                                        i += tamLetDict - 1;
+                                        //console.log("tamLetDict: " + tamLetDict);
+                                        swt = true;
+
+                                    };
+                                }
                             }
                         }
                     }
                 }
             }
         })
-        if(!swt){
+        if (!swt) {
             //console.log("i: " + i);
-            txtOut+=textin[i];            
-        }        
+            txtOut += textin[i];
+        }
 
     }
 
@@ -99,8 +103,8 @@ function desencriptar() {
 
 }
 
-function copiar(){
-    
+function copiar() {
+
     document.getElementById('btnCopy').style.display = 'inline';
     var content = document.getElementById('btnCopy');
     content.select();
